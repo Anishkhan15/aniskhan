db04ef3d932f98980728363ce1dfd9dfbda13549
Anishkhan15 <ka2955159@gmail.com>
2025-06-25 21:30:04 +0530

    Initial commit

--- src/Components/Experience.jsx
+++ src/Components/Experience.jsx
@@ -20,9 +20,9 @@ const Experience = () => {
                 - <span className="text-sm text-purple-200">{experience.company}</span>
               </h6>
               <p className="mb-4 text-neutral-400">{experience.description}</p>
-              {experience.technologies.map((tech, index)=>(
+              {/* {experience.technologies.map((tech, index)=>(
                 <span key={index} className="mr-2 mt-4 rounded bg-neutral-900 px-2 py-1 text-sm font-medium text-purple-800">{tech}</span>
-              ))}
+              ))} */}
             </motion.div>
           </div>
         ))}
